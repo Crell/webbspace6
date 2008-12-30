730e04cd727d983c67fbe46f23716262694a62d7
Larry Garfield <larry@garfieldtech.com>
2008-12-30 06:26:07 +0000

    - Fix Javascript for webbtopic to run and be Drupal 6-ish about it. - Move table dropping to its own update hook.

--- sites/default/modules/webbtopic/webbtopic.js
+++ sites/default/modules/webbtopic/webbtopic.js
@@ -1,16 +1,13 @@
-if (Drupal.jsEnabled) {
-  $(document).ready(function() {
-    // get all attributes of span tag
-    $('span.webbtopic-topic').each ( function() {
-      var a = $("<a>" + $(this).text() + "</a>")
-      .attr('href', '#')
-      .addClass($(this).attr('class'))
-      .bind('click', function(event) {
-        event.preventDefault();
-        var input = $(this).parent().siblings('input');
-        input.val($(this).text());
-      }); // end bind
-      $(this).before(a).remove();
-    }); // end span.webbtopic-topic
+Drupal.behaviors.webbtopic = function (context) {
+  $('.webbtopic-topic:not(.webbtopic-processed)', context).addClass('webbtopic-processed').each(function () {
+    var a = $("<a>" + $(this).text() + "</a>")
+    .attr('href', '#')
+    .addClass($(this).attr('class'))
+    .bind('click', function(event) {
+      event.preventDefault();
+      var input = $(this).parent().siblings('input');
+      input.val($(this).text());
+    }); // end bind
+    $(this).before(a).remove();
   });
-}
+};

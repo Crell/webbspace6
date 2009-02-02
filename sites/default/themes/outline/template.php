6d98bd16b3c7375cb92bc6855820dab9acd66d89
Larry Garfield <larry@garfieldtech.com>
2009-02-02 00:15:46 +0000

    - Tab coloring is done.  Position is still not quite right.

--- sites/default/themes/outline/template.php
+++ sites/default/themes/outline/template.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Implementation of HOOK_theme().
@@ -11,4 +11,18 @@ function outline_theme(&$existing, $type, $theme, $path) {
   */
   // @TODO: Needs detailed comments. Patches welcome!
   return $hooks;
+}
+
+/**
+ * Override or insert variables into the page templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("page" in this case.)
+ */
+function outline_preprocess_page(&$vars, $hook) {
+
+	//dpm($vars['tabs']);
+  //$vars['tabs'] = '';
 }
